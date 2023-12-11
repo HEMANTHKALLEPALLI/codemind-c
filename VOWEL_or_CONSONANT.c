68d650a64ce5c22045ec93bcb65d87bd0a133961
HEMANTHKALLEPALLI <147487467+HEMANTHKALLEPALLI@users.noreply.github.com>
2023-12-11 16:44:32 +0530

    #include<stdio.h> int main() {     char x;     scanf("%c",&x);     if(x=='a')     {         printf("VOWEL");     }     else if(x=='e')     {         printf("VOWEL");     }     else if(x=='i')     {         printf("VOWEL");     }     else if(x=='o')     {         printf("VOWEL");     }     else if(x=='u')     {         printf("VOWEL");     }     else if(x=='A')     {         printf("VOWEL");     }     else if(x=='E')     {         printf("VOWEL");     }     else if(x=='I')     {         printf("VOWEL");     }     else if(x=='O')     {         printf("VOWEL");     }     else if(x=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    if(x=='a')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='e')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='i')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='o')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='u')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='A')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='E')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='I')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='O')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='U')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
