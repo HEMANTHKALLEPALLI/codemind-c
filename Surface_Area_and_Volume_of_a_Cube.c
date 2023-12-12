e6f5da6dcd69a2e60a6aca4e5b07e5512855441a
HEMANTHKALLEPALLI <147487467+HEMANTHKALLEPALLI@users.noreply.github.com>
2023-12-12 18:54:03 +0530

    #include<stdio.h> int main() {     int area,Volume,side;     scanf("%d",&side);     area=6*side*side;     Volume=side*side*side;     printf("Surface area = %d and Volume = %d",area,Volume); }

---
+++ Surface_Area_and_Volume_of_a_Cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int area,Volume,side;
+    scanf("%d",&side);
+    area=6*side*side;
+    Volume=side*side*side;
+    printf("Surface area = %d and Volume = %d",area,Volume);
+}
