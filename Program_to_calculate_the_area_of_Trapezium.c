9d9c8d7c75829f5b36a542726cd06d5698c2a599
HEMANTHKALLEPALLI <147487467+HEMANTHKALLEPALLI@users.noreply.github.com>
2023-12-12 18:32:38 +0530

    #include<stdio.h> int main() {     float base1,base2,height,area;     scanf("%f%f%f",&base1,&base2,&height);     area=((base1+base2)/2)*height;     printf("%.4f",area);
    }

---
+++ Program_to_calculate_the_area_of_Trapezium.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float base1,base2,height,area;
+    scanf("%f%f%f",&base1,&base2,&height);
+    area=((base1+base2)/2)*height;
+    printf("%.4f",area);
+    
+}
