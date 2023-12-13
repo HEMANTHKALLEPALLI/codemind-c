643707713f0f46d8a8c95499afc8c90593245394
HEMANTHKALLEPALLI <147487467+HEMANTHKALLEPALLI@users.noreply.github.com>
2023-12-13 18:28:00 +0530

    #include<stdio.h> int main() {     int phy,chem,bio,math,cs,per,sum;     scanf("%d%d%d%d%d",&phy,&chem,&bio,&math,&cs);     sum=phy+chem+bio+math+cs;     per=sum/5;     if(per>=90)     {         printf("Grade A");     }     else if(per>=80)     {         printf("Grade B");     }     else if(per>=70)     {         printf("Grade C");     }     else if(per>=60)     {         printf("Grade D");     }     else if(per>=40)     {         printf("Grade E");     }     else     {         printf("Grade F");     } }

---
+++ Grades.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int phy,chem,bio,math,cs,per,sum;
+    scanf("%d%d%d%d%d",&phy,&chem,&bio,&math,&cs);
+    sum=phy+chem+bio+math+cs;
+    per=sum/5;
+    if(per>=90)
+    {
+        printf("Grade A");
+    }
+    else if(per>=80)
+    {
+        printf("Grade B");
+    }
+    else if(per>=70)
+    {
+        printf("Grade C");
+    }
+    else if(per>=60)
+    {
+        printf("Grade D");
+    }
+    else if(per>=40)
+    {
+        printf("Grade E");
+    }
+    else
+    {
+        printf("Grade F");
+    }
+}
