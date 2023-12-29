b889abf4193accf48945240266def790ce844b4e
HEMANTHKALLEPALLI <147487467+HEMANTHKALLEPALLI@users.noreply.github.com>
2023-12-29 20:56:17 +0530

    #include <stdio.h>
    int main() {
        int N, R;
    
        scanf("%d %d", &N, &R);
        for (int i = 1; i <= R; i++) {
            printf("%d x %d = %d
    ", N, i, N * i);
        }
    }

--- Multiplication_Table-2.c
+++ Multiplication_Table-2.c
@@ -3,16 +3,9 @@
 int main() {
     int N, R;
 
-    
     scanf("%d %d", &N, &R);
-
- //   printf("Multiplication table of %d up to %d terms:
-", N, R);
-
     for (int i = 1; i <= R; i++) {
         printf("%d x %d = %d
 ", N, i, N * i);
     }
-
-    return 0;
 }
